311792317ca97cc771d36a317c6060f483e8ef73
Munir Wanis <fallen.angel.pp@gmail.com>
2017-03-01 21:57:18 -0300

    :construction: First Commit

---
+++ RetroCalculator/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  RetroCalculator
+//
+//  Created by Munir Wanis on 01/03/17.
+//  Copyright © 2017 Munir Wanis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBAction func zeroBtnPressed(_ sender: Any) {
+    }
+    @IBAction func oneBtnPressed(_ sender: Any) {
+    }
+    @IBAction func twoBtnPressed(_ sender: Any) {
+    }
+    @IBAction func threeBtnPressed(_ sender: Any) {
+    }
+    @IBAction func fourBtnPressed(_ sender: Any) {
+    }
+    @IBAction func fiveBtnPressed(_ sender: Any) {
+    }
+    @IBAction func sixBtnPressed(_ sender: Any) {
+    }
+    @IBAction func sevenBtnPressed(_ sender: Any) {
+    }
+    @IBAction func eightBtnPressed(_ sender: Any) {
+    }
+    @IBAction func nineBtnPressed(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
